83373c094f16869aebb4af92f326be40828724c5
Anders Asplund <aasplund@gmail.com>
2012-05-14 21:22:41 +0200

    Clean up code

--- src/main/java/se/callistaenterprise/rest/SurveyResource.java
+++ src/main/java/se/callistaenterprise/rest/SurveyResource.java
@@ -1,9 +1,6 @@
 package se.callistaenterprise.rest;
 
-import java.net.URI;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
